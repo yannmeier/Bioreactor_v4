05bc7a57ec53270995bcb991839481888d21c8f3
lpatiny <gb252243>
2017-01-08 14:28:11 +0100

    Add bash script to generate BOM.tsv

--- makeBomCsv.sh
+++ makeBomCsv.sh
@@ -2,6 +2,6 @@
 
 grep -E '^\|[^\|]*\| [1-9]' BOM.md | sed -r "s/ *\| */,/g" | sed -r "s/,([^,]*),([^,]*),([^,]*).*/\1\t\2\t\3/" |  sort > BOM.tsv
 
+TOTAL=`cut -f2,3 BOM.tsv | sed -r "s/(.*)\t(.*)/ (\1 * \2) /g" | sed -r "s/  */ /g" | tr '\n' '+' | sed -r "s/^ (.*) \+$/\1\n/" | bc`
 
-
-cut -f2,3 BOM.tsv | sed -r "s/(.*)\t(.*)/ (\1 * \2) /g" | sed -r "s/  */ /g" | tr '\n' '+' | sed -r "s/\+$//" | bc
+echo "Price total: $TOTAL"
