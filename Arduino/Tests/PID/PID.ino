0a56994db34f368d906369ae965124f2c1a44f00
lpatiny <luc@patiny.com>
2017-02-03 19:08:58 +0100

    add some test

---
+++ Arduino/Tests/PID/PID.ino
@@ -0,0 +1,22 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 6;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+analogWrite(led, 150);
+delay(3000);
+}
