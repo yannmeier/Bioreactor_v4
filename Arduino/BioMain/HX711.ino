349a1d22915dd77db111cf0c849308013b2acc28
lpatiny <luc@patiny.com>
2017-02-07 13:28:24 +0100

    Improve the Weight management
    Each process should decide for himself if it is active or not
    A process like 'hx711' should not start of stop another process
    
    There are 2 important variables:
    
    PARAM_ERROR that contains all the errors and can be used by a process
    to decide if it is actuive or not (for PID there should be no error)
    
    PARAM_STATUS that contains the status of all the functions of the bioreactor
    
    Some helper methods have been added:
    * start(flag)
    * stop(flag)
    * isRunning(flag)
    * isEnabled(flag)
    
    The user is also able to enable / disabled some process with the variable
    AZ

--- Arduino/BioMain/HX711.ino
+++ Arduino/BioMain/HX711.ino
@@ -4,16 +4,6 @@
 HX711 scale(WEIGHT_DATA, WEIGHT_CLK);
 
 
-#define WEIGHT_STATUS_NORMAL    0
-#define WEIGHT_STATUS_WAITING   1
-#define WEIGHT_STATUS_EMPTYING  2
-#define WEIGHT_STATUS_FILLING   3
-#define WEIGHT_STATUS_STANDBY   4
-#define WEIGHT_STATUS_ERROR     7
-
-#define EVENT_LOGGING
-
-
 #ifdef DEBUG_WEIGHT
 NIL_WORKING_AREA(waThreadWeight, 96);
 #else
@@ -23,34 +13,34 @@ NIL_WORKING_AREA(waThreadWeight, 56); // minimum of 32 !
 
 
 NIL_THREAD(ThreadWeight, arg) {
+  nilThdSleepMilliseconds(1234); // wait a little bit not everything starts at once
+
   /********************************************
                initialisation
   ********************************************/
   int weight;
 
-  byte weight_status = 0;
-  byte previous_status = WEIGHT_STATUS_ERROR;
-  unsigned long tsinceLastEvent = 0;
-  unsigned long lastCycleMillis = millis(); // when was the last food cycle
-  all_off();                              //clear flags, shut down pumps
-  nilThdSleepMilliseconds(2000);
-
-  //get to the last log status, worst case: just 18 seconds lag with current state
-  if (getParameter(PARAM_WEIGHT_STATUS) != -1) {
-    weight_status = (((uint16_t)getParameter(PARAM_WEIGHT_STATUS)) >> 13);
-    tsinceLastEvent = (((uint16_t)getParameter(PARAM_WEIGHT_STATUS)) & 0b0001111111111111) * 60000;
-  }
-  else
-    weight_status = WEIGHT_STATUS_ERROR;
+  long timeLastEvent = millis() - getParameter(PARAM_WEIGHT_SINCE_LAST_EVENT) * 60000;
 
   /********************************************
                Thread Loop
   ********************************************/
   while (true) {
+    setPumps();
     nilThdSleepMilliseconds(1000);
+
+    int sinceLastEvent = (int)((millis() - timeLastEvent) / 60000);
 
     weight = getWeight(); //sensor read
     setParameter(PARAM_WEIGHT, weight);
+
+    if (! isRunning(FLAG_FOOD_CONTROL) || // Foold control is currently disabled
+        ! isEnabled(FLAG_FOOD_CONTROL)) { // Food control is disabled
+      stop(FLAG_RELAY_FILLING);    //filling  OFF
+      stop(FLAG_RELAY_EMPTYING);   //emptying OFF
+      continue;
+    }
+
     // are we outside ranges ?
     if (weight < (0.80 * getParameter(PARAM_WEIGHT_MIN)) || weight > (1.20 * getParameter(PARAM_WEIGHT_MAX))) {
       saveAndLogError(true, MASK_WEIGHT_ERROR);
@@ -58,169 +48,43 @@ NIL_THREAD(ThreadWeight, arg) {
       saveAndLogError(false, MASK_WEIGHT_ERROR);
     }
 
+    if (isError(MASK_WEIGHT_ERROR)) {  // weight outside range
+#ifdef DEBUG_WEIGHT
+      Serial.print(F("Weight disabled:"));
+      Serial.println(weight);
+#endif
+      stop(FLAG_RELAY_FILLING);     //filling  OFF
+      stop(FLAG_RELAY_EMPTYING);    //emptying OFF
+      continue;
+    }
 
 
-
-
-
-
-    if (weight_status != WEIGHT_STATUS_ERROR) previous_status = weight_status;
-
-
-    /***********************************************
-             Standby and Error management
-    ************************************************/
-    //flag down stands for deactivated weight control
-    //if weight control is on this first loop manages the standby and error cases (wake-up cases)
-    if (! getStatus(FLAG_FOOD_CONTROL)) {
-      weight_status = WEIGHT_STATUS_STANDBY;
+    if (isRunning(FLAG_SEDIMENTATION)) {
+      if (sinceLastEvent >= getParameter(PARAM_SEDIMENTATION_TIME)) {  //switch to Emptying
+        stop(FLAG_SEDIMENTATION);
+        start(FLAG_RELAY_EMPTYING);
+        timeLastEvent = millis();
+      }
+    } else if (isRunning(FLAG_RELAY_EMPTYING)) {
+      if (weight <= getParameter(PARAM_WEIGHT_MIN)) {      //switch fo Filling
+        stop(FLAG_RELAY_EMPTYING);
+        start(FLAG_RELAY_FILLING);
+      }
+    } else if (isRunning(FLAG_RELAY_FILLING)) {
+      if (weight >= getParameter(PARAM_WEIGHT_MAX)) {
+        stop(FLAG_RELAY_FILLING);
+        timeLastEvent = millis();
+      }
     } else {
-
-      if (weight_status == WEIGHT_STATUS_STANDBY) {
-        weight_status = WEIGHT_STATUS_NORMAL;
-#ifdef DEBUG_WEIGHT
-        Serial.println(F("sb>ok"));
-#endif
-      }
-      //safety measures
-      if (weight < (0.80 * getParameter(PARAM_WEIGHT_MIN)) || weight > (1.20 * getParameter(PARAM_WEIGHT_MAX))) {
-
-#ifdef EVENT_LOGGING
-        if (weight_status != WEIGHT_STATUS_ERROR) writeLog(EVENT_WEIGHT_FAILURE, weight);
-#endif
-
-        all_off();
-        weight_status = WEIGHT_STATUS_ERROR;
-
-#ifdef DEBUG_WEIGHT
-        Serial.print(F("Weight error:"));
-        Serial.println(weight);
-#endif
-
-      } else if (weight_status == WEIGHT_STATUS_ERROR) {
-#ifdef EVENT_LOGGING
-        writeLog(EVENT_WEIGHT_BACK_TO_NORMAL, 0);
-#endif
-        if (previous_status == WEIGHT_STATUS_ERROR) weight_status = WEIGHT_STATUS_NORMAL;
-        else weight_status = previous_status;
-#ifdef DEBUG_WEIGHT
-        Serial.println(F("er>ok"));
-#endif
+      if (sinceLastEvent >= getParameter(PARAM_FILLED_TIME)) {          //switch to Sedimentation
+        start(FLAG_SEDIMENTATION);
+        timeLastEvent = millis();
       }
     }
 
-
-    setParameter(PARAM_WEIGHT_STATUS, (((uint16_t)(tsinceLastEvent / 60000)) | ((uint16_t)(weight_status << 13))));
-
-#ifdef DEBUG_WEIGHT
-    Serial.print(weight_status);
-    Serial.print(F(" "));
-    Serial.println(weight);
-#endif
-
-    /**************************************
-           Weight State Machine
-    **************************************/
-    switch (weight_status) {
-
-      case WEIGHT_STATUS_NORMAL:
-        all_off();
-        setParameterBit(PARAM_STATUS, FLAG_PH_CONTROL);        //pH      ON
-        setParameterBit(PARAM_STATUS, FLAG_STEPPER_CONTROL);   //stepper ON
-        setParameterBit(PARAM_STATUS, FLAG_PID_CONTROL);       //PID ON
-        if (( (uint16_t)(tsinceLastEvent / 60000)) >= getParameter(PARAM_FILLED_TIME)) {          //switch to Sedimentation
-          weight_status = WEIGHT_STATUS_WAITING;
-          tsinceLastEvent = 0;
-#ifdef EVENT_LOGGING
-          writeLog(EVENT_PUMPING_WAITING, 0);
-          writeLog(EVENT_MOTOR_STOP, 0);
-#endif
-        }
-        break;
-
-      case WEIGHT_STATUS_WAITING:
-        all_off();
-        if (( (uint16_t)(tsinceLastEvent / 60000)) >= getParameter(PARAM_SEDIMENTATION_TIME)) {  //switch to Emptying
-          weight_status = WEIGHT_STATUS_EMPTYING;
-          tsinceLastEvent = 0;
-#ifdef EVENT_LOGGING
-          writeLog(EVENT_PUMPING_EMPTYING_START, 0);
-          nilThdSleepMilliseconds(10);
-#endif
-        }
-        break;
-
-      case WEIGHT_STATUS_EMPTYING:
-        all_off();
-        setParameterBit(PARAM_STATUS, FLAG_RELAY_EMPTYING);  //emptying ON
-        if (weight <= getParameter(PARAM_WEIGHT_MIN)) {      //switch fo Filling
-#ifdef DEBUG_WEIGHT
-          Serial.print(F("empty:"));
-          Serial.println(weight);
-#endif
-          weight_status = WEIGHT_STATUS_FILLING;
-          tsinceLastEvent = 0;
-#ifdef EVENT_LOGGING
-          writeLog(EVENT_PUMPING_EMPTYING_STOP, 0);
-          nilThdSleepMilliseconds(10);
-#endif
-          // TURN ON ROTATION
-#ifdef EVENT_LOGGING
-          writeLog(EVENT_MOTOR_START, 0);
-          nilThdSleepMilliseconds(10);
-#endif
-          // turn on filling pump
-#ifdef EVENT_LOGGING
-          writeLog(EVENT_PUMPING_FILLING_START, 0);
-          nilThdSleepMilliseconds(10);
-#endif
-        }
-        break;
-
-      case WEIGHT_STATUS_FILLING:
-        all_off();
-        setParameterBit(PARAM_STATUS, FLAG_RELAY_FILLING);     //filling  ON
-        setParameterBit(PARAM_STATUS, FLAG_STEPPER_CONTROL);   //stepper  ON
-        setParameterBit(PARAM_STATUS, FLAG_PID_CONTROL);       //PID ON
-
-        if (weight >= getParameter(PARAM_WEIGHT_MAX)) {
-#ifdef DEBUG_WEIGHT
-          Serial.print(F("full:"));
-          Serial.println(weight);
-#endif
-          weight_status = WEIGHT_STATUS_NORMAL;
-          tsinceLastEvent = 0;
-
-#ifdef EVENT_LOGGING
-          writeLog(EVENT_PUMPING_FILLING_STOP, 0);  //need to move the event loggers
-          nilThdSleepMilliseconds(10);
-#endif
-        }
-        break;
-
-      case WEIGHT_STATUS_ERROR:
-        all_off();
-    }
-
-
-    // Food control with 2 pumps
-#ifdef FOOD_IN
-    digitalWrite(FOOD_IN, getParameterBit(PARAM_STATUS, FLAG_RELAY_FILLING));
-    delay(10);
-#endif
-#ifdef FOOD_OUT
-    digitalWrite(FOOD_OUT, getParameterBit(PARAM_STATUS, FLAG_RELAY_EMPTYING));
-    delay(10);
-#endif
-    tsinceLastEvent += (millis() - lastCycleMillis);
-    lastCycleMillis = millis();
+    setParameter(PARAM_WEIGHT_SINCE_LAST_EVENT, sinceLastEvent);
   }
 }
-
-
-
-
-
 
 
 
@@ -265,6 +129,11 @@ int getWeight() { // we can not avoid to have some errors measuring the weight
       nilThdSleepMilliseconds(10);
     }
   }
+#ifdef DEBUG_WEIGHT
+  Serial.print(weightStatus);
+  Serial.print(F(" "));
+  Serial.println(weight);
+#endif
   return weight / counter / 100;
 }
 
@@ -314,21 +183,16 @@ void processWeightCommand(char command, char* data, Print * output) {
     Utilities
 *********************/
 
-void all_off() {
+void setPumps() {
   //stop pumping on startup and put down the flags
 #ifdef FOOD_IN
   pinMode(FOOD_IN, OUTPUT);
-  digitalWrite(FOOD_IN, LOW);
+  digitalWrite(FOOD_IN, getStatus(FLAG_RELAY_FILLING));
 #endif
 #ifdef FOOD_OUT
   pinMode(FOOD_OUT, OUTPUT);
-  digitalWrite(FOOD_OUT, LOW);
+  digitalWrite(FOOD_OUT, getStatus(FLAG_RELAY_EMPTYING));
 #endif
-  clearParameterBit(PARAM_STATUS, FLAG_STEPPER_CONTROL); //stepper  OFF
-  clearParameterBit(PARAM_STATUS, FLAG_PH_CONTROL);      //pH       OFF
-  clearParameterBit(PARAM_STATUS, FLAG_RELAY_FILLING);   //filling  OFF
-  clearParameterBit(PARAM_STATUS, FLAG_RELAY_EMPTYING);  //emptying OFF
-  clearParameterBit(PARAM_STATUS, FLAG_PID_CONTROL);     //pid      OFF
 }
 
 
--- Arduino/BioMain/Stepper.ino
+++ Arduino/BioMain/Stepper.ino
@@ -1,106 +1,130 @@
 #ifdef STEPPER
 /****************************
- *  THREAD STEPPER MOTOR
- *  This is the thread controlling the motor. It should have a high priority 
- *  as it is called very often and is short. It controls the sequence with the pin PWM and IO
- *  of the port.
- *  The sequence for turning the motor turn is :
- *  RED-GREEN-BLUE-BLACK => win turn clockwise (top view) where :
- *  RED = {PWM=LOW, IO=LOW}
- *  BLUE = {PWM=LOW, IO=HIGH}
- *  BLACK = {PWM=HIGH, IO=HIGH}
- *  RED = {PWM=HIGH, IO=LOW}
+    THREAD STEPPER MOTOR
+    This is the thread controlling the motor. It should have a high priority
+    as it is called very often and is short. It controls the sequence with the pin PWM and IO
+    of the port.
+    The sequence for turning the motor turn is :
+    RED-GREEN-BLUE-BLACK => win turn clockwise (top view) where :
+    RED = {PWM=LOW, IO=LOW}
+    BLUE = {PWM=LOW, IO=HIGH}
+    BLACK = {PWM=HIGH, IO=HIGH}
+    RED = {PWM=HIGH, IO=LOW}
  ******************************/
 
 //We define here the number of step executed during every call to the thread
 //#define NB_STEP_CALL  10000 // Maximum 65535 !!!!
-byte STEPPER_TAB[]= STEPPER;
+byte STEPPER_TAB[] = STEPPER;
 
 
 void stopStepper() {
-    PORTB &=~(STEPPER_TAB[0] | STEPPER_TAB[1]);
-    PORTF &=~(STEPPER_TAB[2] | STEPPER_TAB[3]);
+  PORTB &= ~(STEPPER_TAB[0] | STEPPER_TAB[1]);
+  PORTF &= ~(STEPPER_TAB[2] | STEPPER_TAB[3]);
 }
 
+#ifdef BEFORE_43
 
-void executeStep(uint16_t numberSteps, boolean forward) {
-  DDRB |= (STEPPER_TAB[0]|STEPPER_TAB[1]) ;
-  DDRF |= (STEPPER_TAB[2]|STEPPER_TAB[3]) ;
-  uint8_t counter=0;
-  while (numberSteps>0) {
-    
-    if(getParameterBit(PARAM_STATUS, FLAG_STEPPER_CONTROL)==0){
+void executeStep(uint16_t numberSteps, boolean forward, byte port1, byte port2) {
+  uint8_t counter = 0;
+  while (numberSteps > 0) {
+
+    if (! isRunning(FLAG_STEPPER_CONTROL) || ! isEnabled(FLAG_STEPPER_CONTROL)) { // PID is disabled
       stopStepper();
-      return; 
-   }
-    
-    numberSteps--;
-    if (forward) counter++;
-    else counter--;
-    
-    if((getParameter(PARAM_STEPPER_SPEED)%11)!=0){
-    switch (counter % 4) {
-    case 0:
-      PORTB|= STEPPER_TAB[0];
-      break;
-    case 1:
-      PORTB|= STEPPER_TAB[1];    // 1 or 2
-      break;
-    case 3:   // 2 or 3
-      PORTF|= STEPPER_TAB[2];
-      break;
-    case 2:
-      PORTF |= STEPPER_TAB[3];
-      break;
+      return;
     }
- 
-    nilThdSleepMilliseconds(11-getParameter(PARAM_STEPPER_SPEED)%11);
 
-    stopStepper();
-    
-    } else  nilThdSleepMilliseconds(100);  
-  }
-}
+    if (isError()) { // any error we should stop heating !
+      stopStepper();
+      return;
+    }
 
+    if (isRunning(FLAG_RELAY_EMPTYING) || isRunning(FLAG_SEDIMENTATION)) {
+      stopStepper();
+      return;
+    }
 
-
-void oldExecuteStep(uint16_t numberSteps, boolean forward, byte port1, byte port2) {
-  uint8_t counter=0;
-  while (numberSteps>0) {
-    
-    if(getParameterBit(PARAM_STATUS, FLAG_STEPPER_CONTROL)==0) return;
-    
     numberSteps--;
     if (forward) counter++;
     else counter--;
-    
-    if((getParameter(PARAM_STEPPER_SPEED)%101)!=0){
-        switch (counter % 4) {
+
+    if ((getParameter(PARAM_STEPPER_SPEED) % 101) != 0) {
+      switch (counter % 4) {
         case 0:
           //This is RED & BLUE
           digitalWrite(port1, LOW);
-          digitalWrite(port2,LOW);
+          digitalWrite(port2, LOW);
           break;
         case 1:   // 1 or 2
           //This is BLUE
           digitalWrite(port1, LOW);
-          digitalWrite(port2,HIGH);
+          digitalWrite(port2, HIGH);
           break;
         case 2:   // 2 or 3
           //This is Black
           digitalWrite(port1, HIGH);
-          digitalWrite(port2,HIGH);
+          digitalWrite(port2, HIGH);
           break;
         case 3:   // 3 or 1
           //This is Green
           digitalWrite(port1, HIGH);
-          digitalWrite(port2,LOW);
+          digitalWrite(port2, LOW);
           break;
-        }
-        nilThdSleepMilliseconds(1+(10-getParameter(PARAM_STEPPER_SPEED)%11));      
-    }else  nilThdSleepMilliseconds(100);  
+      }
+      nilThdSleepMilliseconds(1 + (10 - getParameter(PARAM_STEPPER_SPEED) % 11));
+    } else  nilThdSleepMilliseconds(100);
   }
 }
+#else
+void executeStep(uint16_t numberSteps, boolean forward) {
+  DDRB |= (STEPPER_TAB[0] | STEPPER_TAB[1]) ;
+  DDRF |= (STEPPER_TAB[2] | STEPPER_TAB[3]) ;
+  uint8_t counter = 0;
+  while (numberSteps > 0) {
+
+    if (! isRunning(FLAG_STEPPER_CONTROL) || ! isEnabled(FLAG_STEPPER_CONTROL)) { // PID is disabled
+      stopStepper();
+      return;
+    }
+
+    if (isError()) { // any error we should stop heating !
+      stopStepper();
+      return;
+    }
+
+    if (isRunning(FLAG_RELAY_EMPTYING) || isRunning(FLAG_SEDIMENTATION)) {
+      stopStepper();
+      return;
+    }
+
+    numberSteps--;
+    if (forward) counter++;
+    else counter--;
+
+    if ((getParameter(PARAM_STEPPER_SPEED) % 11) != 0) {
+      switch (counter % 4) {
+        case 0:
+          PORTB |= STEPPER_TAB[0];
+          break;
+        case 1:
+          PORTB |= STEPPER_TAB[1];   // 1 or 2
+          break;
+        case 3:   // 2 or 3
+          PORTF |= STEPPER_TAB[2];
+          break;
+        case 2:
+          PORTF |= STEPPER_TAB[3];
+          break;
+      }
+
+      nilThdSleepMilliseconds(11 - getParameter(PARAM_STEPPER_SPEED) % 11);
+
+      stopStepper();
+
+    } else  nilThdSleepMilliseconds(100);
+  }
+}
+#endif
+
 
 
 NIL_WORKING_AREA(waThreadStepper, 0);
@@ -110,26 +134,26 @@ NIL_THREAD(ThreadStepper, arg) {
   uint8_t count = 0;
 
 
-  #ifdef BEFORE_43
-  for (byte i=0; i<sizeof(STEPPER_TAB); i++) {
+#ifdef BEFORE_43
+  for (byte i = 0; i < sizeof(STEPPER_TAB); i++) {
     pinMode(STEPPER_TAB[i], OUTPUT);
   }
-  #else
-    stopStepper();
-    DDRB |= (STEPPER_TAB[0] | STEPPER_TAB[1]) ;
-    DDRF |= (STEPPER_TAB[2] | STEPPER_TAB[3]) ;
-  #endif
+#else
+  stopStepper();
+  DDRB |= (STEPPER_TAB[0] | STEPPER_TAB[1]) ;
+  DDRF |= (STEPPER_TAB[2] | STEPPER_TAB[3]) ;
+#endif
 
 
 
 
   while (true) {
     //first a check is performed on the motor status
-    #ifdef BEFORE_43
-      oldExecuteStep((uint16_t)getParameter(PARAM_STEPPER_STEPS), forward, STEPPER_TAB[1],STEPPER_TAB[0]);
-    #else
-      executeStep((uint16_t)getParameter(PARAM_STEPPER_STEPS), forward);
-    #endif
+#ifdef BEFORE_43
+    executeStep((uint16_t)getParameter(PARAM_STEPPER_STEPS), forward, STEPPER_TAB[1], STEPPER_TAB[0]);
+#else
+    executeStep((uint16_t)getParameter(PARAM_STEPPER_STEPS), forward);
+#endif
     forward = !forward;
     nilThdSleepMilliseconds(1000);
   }
