8adfbbd2cca070bfb3c42240ebe22f2ec096da5c
lpatiny <luc@patiny.com>
2017-09-06 16:22:32 -0500

    update bom

--- docs/making/bom/makeBomCsv.sh
+++ docs/making/bom/makeBomCsv.sh
@@ -5,6 +5,7 @@ grep -E '^\|[^\|]*\| [1-9]' index.md | sed -E "s/ *\| */,/g" | sed -E "s/,([^,]*
 awk '
 BEGIN {
     FS = "\t";
+    FS="\t";
     total = 0;
     price = 0;
     description = "";
@@ -12,7 +13,7 @@ BEGIN {
 {
     if ($1 != description) {
         if (description != "") {
-            print description "\t" total "\t" price
+            print description,total,price
         }
         description = $1;
         price = $3;
@@ -23,7 +24,7 @@ BEGIN {
 }
 ' > BOM.tsv
 
-
+exit
 TOTAL=`cut -f2,3 BOM.tsv | sed -E "s/(.*)\t(.*)/ (\1 * \2) /g" | sed -E "s/  */ /g" | tr '\n' '+' | sed -E "s/^ (.*) \+$/\1\n/" | bc`
 
 echo "Price total: $TOTAL"
